f8f00ba5aa553fdbdf34f3d3342a6a00c3d46a2c
raymcfarlane <122712555+raymcfarlane@users.noreply.github.com>
2023-07-09 13:52:44 -0400

    conn

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/social_network_DB');
+const connectionString =
+  process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/social_network_DB';
+
+connect(connectionString);
 
 module.exports = connection;
